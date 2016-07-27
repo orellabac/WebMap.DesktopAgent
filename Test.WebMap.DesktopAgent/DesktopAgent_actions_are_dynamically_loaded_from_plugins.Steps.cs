7e875cfaed464588d76cacc6a41d0811fe153ab7
Mauricio Rojas <orellabac@hotmail.com>
2016-07-27 11:02:35 -0600

    Starting test cases

---
+++ Test.WebMap.DesktopAgent/DesktopAgent_actions_are_dynamically_loaded_from_plugins.Steps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LightBDD;
+using NUnit.Framework;
+
+namespace Test.WebMap.DesktopAgent
+{
+    public partial class DesktopAgent_actions_are_dynamically_loaded_from_plugins : FeatureFixture
+    {
+        private void Given_a_request_for_Plugin_HelloWorldPluginNotFound_Action_CustomAction() { }
+
+
+        private void When_plugin_is_not_installed()
+        {
+            
+        }
+
+        private void Then_response_should_be_Status_error_Info_plugin_not_found()
+        {
+        }
+
+
+    }
+}
